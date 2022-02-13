dca16d19a0c009341d62fac2bfd8cd310419b887
christinarone <96158586+christinarone@users.noreply.github.com>
2022-02-13 14:11:47 -0600

    completed

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
     // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
-
-    let arrayOfTodos = [
-        {
-        "userId": 14,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-    },
-    {
-        "userId": 20,
-        "id": 2,
-        "title": "delectus aut autem",
-        "completed": false
-    }]
     
     const fetchTodos = () => {
         fetch('https://jsonplaceholder.typicode.com/todos')
@@ -28,8 +14,34 @@ const populateTodos = () => {
     for (let i = 0; i < arrayOfTodos.length; i++) {
         let todoList = document.getElementById("todo-list")
         let element = document.createElement("li")
-        let text = document.createTextNode(arrayOfTodos[0].title)
+        let text = document.createTextNode(arrayOfTodos[i].title)
         element.appendChild(text)
         todoList.appendChild(element)
     }
 }
+
+const filterTodos = () => {
+//get the value from DOM and set user ID, then filter through the array and remove it all except the ID that matches//
+
+const todoId = document.getElementById('UserIdIn').value; 
+var result = arrayOfTodos.filter(user => user.userId == todoId)
+    console.log("userId is:"+todoId);
+    console.log(result)
+
+}
+function clearTodos(){
+    let todoList = document.getElementById("todo-list")
+    todoList.innerHTML=""
+    }
+
+
+function setUserId(){
+
+}
+
+function thisID(){
+    
+}
+
+
+
